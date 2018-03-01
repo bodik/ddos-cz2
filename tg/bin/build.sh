46c06ccd39343d81ddf25d057e7678e9685f9c51
root <root@template-debian-stretch.cesnet.cz>
2018-03-01 22:13:06 +0100

    build script refactoring

---
+++ tg/bin/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+
+# dependencies
+apt-get install gcc cpp make pkg-config flex bison
+apt-get install libnacl-dev libz-dev libncurses5-dev libnl-3-dev
+
+
+# build
+BUILD_AREA=/tmp/build_area
+mkdir -p $BUILD_AREA
+cd $BUILD_AREA || exit 1
+
+if [ ! -d netsniff-ng ]; then
+	git clone https://github.com/netsniff-ng/netsniff-ng
+	cd netsniff-ng
+else
+	cd netsniff-ng
+	git pull
+fi
+./configure
+make trafgen ifpps
--- tg/build/install.pp
+++
@@ -1,8 +0,0 @@
-
-$packages = [
-	"gcc", "cpp", "make", "pkg-config", "flex", "bison",
-	"libnacl-dev", "libz-dev", "libncurses5-dev", "libnl-3-dev"
-]
-
-package { $packages: ensure => installed }
-
