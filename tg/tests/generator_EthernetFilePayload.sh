aa49e72be1a38d2631fe337665bbd6b569ca7df2
root <root@template-debian-stretch.cesnet.cz>
2018-03-06 11:50:45 +0100

    import tests/generator_EthernetFilePayload.sh

---
+++ tg/tests/generator_EthernetFilePayload.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. $(dirname $(readlink -f $0))/common.sh
+
+
+
+TESTPAYLOAD=$(mktemp /tmp/tg2_generator_filepayload_XXXXXX)
+echo "0,0,0,0,0,0,0,0,0,0" > $TESTPAYLOAD
+
+
+
+run_and_display EthernetFilePayload --eth_protocol "0x0000" --filename ${TESTPAYLOAD}
+cat ${TESTID_FILE_DISPLAYED}
+
+
+
+check_value "802.3.len: 0"
+
+
+
+rm -f ${TESTPAYLOAD}
+cleanup
+rreturn 0 "$0"
