a12cf0ab6dcad8798d290ecdfac58640b3b02cbe
root <root@tg1.flab.cesnet.cz>
2018-06-19 14:06:27 +0200

    randomized low byte of flow label

--- tg/tests/generator_Udp6RandomPayload.sh
+++ tg/tests/generator_Udp6RandomPayload.sh
@@ -5,6 +5,7 @@
 
 ETH_SOURCE_MAC="$(random_mac)"
 ETH_DESTINATION_MAC="$(random_mac)"
+IP6_FLOW_LABEL=$(random_byte)
 IP6_HOP_LIMIT=3
 IP6_SOURCE=$(random_ipv6)
 IP6_DESTINATION=$(random_ipv6)
@@ -18,6 +19,7 @@ run_and_display Udp6RandomPayload \
 	--num 1 \
 	--eth_source_mac ${ETH_SOURCE_MAC} \
 	--eth_destination_mac ${ETH_DESTINATION_MAC} \
+	--ip6_flow_label ${IP6_FLOW_LABEL} \
 	--ip6_hop_limit ${IP6_HOP_LIMIT} \
 	--ip6_source_address ${IP6_SOURCE} \
 	--ip6_destination_address ${IP6_DESTINATION} \
@@ -27,9 +29,11 @@ run_and_display Udp6RandomPayload \
 
 
 
+check_value "IPv6.version: 6"
+check_value "IPv6.fl: ${IP6_FLOW_LABEL}"
 check_value "IPv6.plen: $((8+${PAYLOAD_LENGTH}))"
-check_value "IPv6.hlim: ${IP6_HOP_LIMIT}"
 check_value "IPv6.nh: 17"
+check_value "IPv6.hlim: ${IP6_HOP_LIMIT}"
 check_value "IPv6.src: ${IP6_SOURCE}"
 check_value "IPv6.dst: ${IP6_DESTINATION}"
 
