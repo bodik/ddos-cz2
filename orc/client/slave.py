9e4a3c2225eeaafd7dca9f55c5e16d7241bf4406
root <root@template-debian-stretch.cesnet.cz>
2018-06-22 16:57:24 +0200

    cleanup

--- orc/client/slave.py
+++ orc/client/slave.py
@@ -24,8 +24,8 @@ class CommunicatorWampSession(autobahn.asyncio.wamp.ApplicationSession):
 	def __init__(self, *args, **kwargs):
 		super(CommunicatorWampSession, self).__init__(*args, **kwargs)
 		self.received = 0
-		self.msgSchema = self.config.extra["msgSchema"]
-		self.communicatorThread = self.config.extra["communicatorThread"]
+		self.msg_schema = self.config.extra["msg_schema"]
+		self.communicator_thread = self.config.extra["communicator_thread"]
 
 	async def onJoin(self, details):
 		def on_message(msg, details):
@@ -39,34 +39,34 @@ class CommunicatorWampSession(autobahn.asyncio.wamp.ApplicationSession):
 
 	def processMessage(self, msg):
 		try:
-			jsonschema.validate(msg, self.msgSchema)
-			valid = True
+			jsonschema.validate(msg, self.msg_schema)
 		except jsonschema.exceptions.ValidationError:
-			valid = False
-		self.log.info("{cls}: {valid} message {msg}", cls=self.__class__.__name__, valid=valid, msg=msg)
+			self.log.warn("{cls}: invalid message {msg}", cls=self.__class__.__name__, msg=msg)
+			return
 
+		self.log.info("{cls}: message {msg}", cls=self.__class__.__name__, msg=msg)
 		self.received += 1
 		if self.received > 30:
 			self.leave()
 
 
 class CommunicatorThread(threading.Thread):
-	def __init__(self, server, realm, msgSchema):
+	def __init__(self, server, realm, msg_schema, log_level="info"):
 		threading.Thread.__init__(self)
 		self.setDaemon(True)
 		self.name = "communicator"
+		self.log = txaio.make_logger() #caveat: mind the semantic slightly differs from python's standard logging
 
 		# mandatory asyncio
 		self.loop = None
-		self.logLevel = "debug"
+		self.log_level = log_level
 
-		# custom handling reconnects
+		# comms
 		self.shutdown = False
-
-		# custom arguments
 		self.server = server
 		self.realm = realm
-		self.msgSchema = msgSchema
+		with open(msg_schema, "r") as ftmp:
+			self.msg_schema = json.loads(ftmp.read())
 
 		# ipc
 		self.inbox = []
@@ -74,7 +74,7 @@ class CommunicatorThread(threading.Thread):
 
 
 	def run(self):
-		logging.info("start %s" % self.name)
+		self.log.info("start %s" % self.name)
 
 		self.loop = asyncio.new_event_loop()
 		asyncio.set_event_loop(self.loop)
@@ -87,35 +87,36 @@ class CommunicatorThread(threading.Thread):
 				if self.loop.is_closed():
 				        asyncio.set_event_loop(asyncio.new_event_loop())
 				        self.loop = asyncio.get_event_loop()
-				runner = autobahn.asyncio.wamp.ApplicationRunner(self.server, realm=self.realm, extra={"communicatorThread": self, "msgSchema": self.msgSchema})
-				coro = runner.run(CommunicatorWampSession, start_loop=False, log_level=self.logLevel)
+				runner = autobahn.asyncio.wamp.ApplicationRunner(self.server, realm=self.realm, extra={"communicator_thread": self, "msg_schema": self.msg_schema})
+				coro = runner.run(CommunicatorWampSession, start_loop=False, log_level=self.log_level)
 				(transport, protocol) = self.loop.run_until_complete(coro)
 				## must not be handled by thread but only by main itself
 				###self.loop.add_signal_handler(signal.SIGTERM, self.loop.stop)
 				try:
 				        self.loop.run_forever()
 				except KeyboardInterrupt:
-				        logging.info("aborted by user")
-				        shutdown = True
+				        self.log.info("aborted by user")
+				        self.shutdown = True
 				if protocol._session:
 				        self.loop.run_until_complete(protocol._session.leave())
 				self.loop.close()
 
 			except Exception as e:
-				logging.error(e)
+				self.log.error(e)
 				try:
 					time.sleep(1)
 				except KeyboardInterrupt:
-					shutdown = True
+					self.shutdown = True
 
-		logging.info("end %s" % self.name)
+		self.log.info("end %s" % self.name)
 
 	def teardown(self):
 		"""called from external objects to singal gracefull teardown request"""
 
-		logging.info("shutting down %s", self.name)
+		self.log.info("shutting down %s" % self.name)
 		self.shutdown = True
 		self.loop.stop()
+
 
 	def send(self, msg):
 		self.outbox.append(msg)
@@ -132,10 +133,10 @@ def parse_arguments():
 	"""parse arguments"""
 
 	parser = argparse.ArgumentParser()
+
 	parser.add_argument("--server", default="ws://127.0.0.1:56000/")
 	parser.add_argument("--realm", default="orc1")
 	parser.add_argument("--schema", default="orcish.schema")
-
 	parser.add_argument("--debug", action="store_true")
 
 	return parser.parse_args()
@@ -154,20 +155,24 @@ def teardown(signum, frame):
 
 
 def main():
-	# args
 	"""main"""
+
+
+	# args
 	log_level = "info"
 	args = parse_arguments()
 	if args.debug:
 		log_level = "debug"
+	txaio.start_logging(level=log_level)
 
 	# startup
-	txaio.start_logging(level=log_level)
-	
 	signal.signal(signal.SIGTERM, teardown)
 	signal.signal(signal.SIGINT, teardown)
-	thread_communicator = CommunicatorThread(args.server, args.realm, {})
+	thread_communicator = CommunicatorThread(args.server, args.realm, args.schema, log_level)
 	thread_communicator.start()
+
+	# there will be a loop
+
 	thread_communicator.join()
 
 
