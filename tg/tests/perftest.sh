9fdcace25fdba08c5888950e92902e0d9bbaf141
root <root@tg2.flab.cesnet.cz>
2018-07-04 20:58:46 +0200

    explicit iface to perftester

--- tg/tests/perftest.sh
+++ tg/tests/perftest.sh
@@ -17,55 +17,57 @@
 #```
 
 
+export NETSTAT_IFACE="eth0"
 export IP4TARGET=""
 export IP6TARGET=""
 
 
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 UdpRandomPayload --ip4_destination_address ${IP4TARGET} --length 0"
+
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 UdpRandomPayload --ip4_destination_address ${IP4TARGET} --length 0"
 sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 UdpRandomPayload --ip4_destination_address ${IP4TARGET} --length 100"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 UdpRandomPayload --ip4_destination_address ${IP4TARGET} --length 100"
 sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 UdpRandomPayload --ip4_source_address drnd --ip4_destination_address ${IP4TARGET} --length 1300"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 UdpRandomPayload --ip4_source_address drnd --ip4_destination_address ${IP4TARGET} --length 1300"
 sleep 60
 
 
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 TcpHeader --ip4_destination_address ${IP4TARGET}"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 TcpHeader --ip4_destination_address ${IP4TARGET}"
 sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 TcpHeader --ip4_destination_address ${IP4TARGET} --tcp_source_port drnd"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 TcpHeader --ip4_destination_address ${IP4TARGET} --tcp_source_port drnd"
 sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 TcpHeader --ip4_source_address drnd --ip4_destination_address ${IP4TARGET} --tcp_source_port drnd"
-sleep 60
-
-
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 IcmpEcho --ip4_destination_address ${IP4TARGET}"
-sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 IcmpEcho --ip4_destination_address ${IP4TARGET} --icmpecho_data $(python -c 'print "A"*2')"
-sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 IcmpEcho --ip4_source_address drnd --ip4_destination_address ${IP4TARGET} --icmpecho_data $(python -c 'print "A"*100')"
-
-
-
-
-
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 Udp6RandomPayload --ip6_destination_address ${IP6TARGET} --length 0"
-sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 Udp6RandomPayload --ip6_destination_address ${IP6TARGET} --length 100"
-sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 Udp6RandomPayload --ip6_source_address drnd --ip6_destination_address ${IP6TARGET} --length 1300"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 TcpHeader --ip4_source_address drnd --ip4_destination_address ${IP4TARGET} --tcp_source_port drnd"
 sleep 60
 
 
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 Tcp6Header --ip6_destination_address ${IP6TARGET}"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 IcmpEcho --ip4_destination_address ${IP4TARGET}"
 sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 Tcp6Header --ip6_destination_address ${IP6TARGET} --tcp_source_port drnd"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 IcmpEcho --ip4_destination_address ${IP4TARGET} --icmpecho_data $(python -c 'print "A"*2')"
 sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 Tcp6Header --ip6_source_address drnd --ip6_destination_address ${IP6TARGET} --tcp_source_port drnd"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 IcmpEcho --ip4_source_address drnd --ip4_destination_address ${IP4TARGET} --icmpecho_data $(python -c 'print "A"*100')"
+
+
+
+
+
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 Udp6RandomPayload --ip6_destination_address ${IP6TARGET} --length 0"
+sleep 60
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 Udp6RandomPayload --ip6_destination_address ${IP6TARGET} --length 100"
+sleep 60
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 Udp6RandomPayload --ip6_source_address drnd --ip6_destination_address ${IP6TARGET} --length 1300"
 sleep 60
 
 
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 Icmp6Echo --ip6_destination_address ${IP6TARGET}"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 Tcp6Header --ip6_destination_address ${IP6TARGET}"
 sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 Icmp6Echo --ip6_destination_address ${IP6TARGET} --icmp6echo_data $(python -c 'print "A"*2')"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 Tcp6Header --ip6_destination_address ${IP6TARGET} --tcp_source_port drnd"
 sleep 60
-metalib/bin/perftest_netstat.py --perftest_time 60 --perftest_cmd "tg/tg2 Icmp6Echo --ip6_destination_address ${IP6TARGET} --icmp6echo_data $(python -c 'print "A"*100')"
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 Tcp6Header --ip6_source_address drnd --ip6_destination_address ${IP6TARGET} --tcp_source_port drnd"
+sleep 60
+
+
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 Icmp6Echo --ip6_destination_address ${IP6TARGET}"
+sleep 60
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 Icmp6Echo --ip6_destination_address ${IP6TARGET} --icmp6echo_data $(python -c 'print "A"*2')"
+sleep 60
+metalib/bin/perftest_netstat.py --netstat_iface ${NETSTAT_IFACE} --perftest_time 60 --perftest_cmd "tg/tg2 Icmp6Echo --ip6_destination_address ${IP6TARGET} --icmp6echo_data $(python -c 'print "A"*100')"
 sleep 60
