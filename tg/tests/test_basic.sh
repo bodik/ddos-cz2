b6eb2995694c23d4938ec4d1dd40ed0d01b92ac1
root <root@template-debian-stretch.cesnet.cz>
2019-01-21 23:31:04 +0100

    test fix, pgroup does not work well in full test suite through main wrapper

--- tg/tests/test_basic.sh
+++ tg/tests/test_basic.sh
@@ -9,35 +9,35 @@ echo "{ 0 }" > $RAWCONFIG
 
 
 
-# test send number of packets
+echo "INFO: test send number of packets"
 NUMBER=$(($(random_byte)+1))
 run_and_display RawConfig --filename ${RAWCONFIG} --num ${NUMBER}
 check_value "scapy_display.summary.count: ${NUMBER}"
 cleanup
 
 
-# test send number of packets for time at specified rate
+echo "INFO: test send number of packets for time at specified rate"
 NUMBER=4
 run_and_display RawConfig --filename ${RAWCONFIG} --time ${NUMBER} --rate 1pps
 check_value "scapy_display.summary.count: ${NUMBER}"
 cleanup
 
 
-# test sigterm handling
+echo "INFO: test sigterm handling"
 NUMBER=3
 run_and_display RawConfig --filename ${RAWCONFIG} --time $((2*${NUMBER})) --rate 1pps &
 sleep ${NUMBER}
-pkill --pgroup $$ --signal TERM --full "/usr/bin/python3 ${BASEDIR}/tg2"
+pkill --newest --signal TERM --full "/usr/bin/python3 ${BASEDIR}/tg2"
 wait
 check_value "scapy_display.summary.count: ${NUMBER}"
 cleanup
 
 
-# test sigint handling
+echo "INFO: test sigint handling"
 NUMBER=3
 run_and_display RawConfig --filename ${RAWCONFIG} --time $((2*${NUMBER})) --rate 1pps &
 sleep ${NUMBER}
-pkill --pgroup $$ --signal INT --full "/usr/bin/python3 ${BASEDIR}/tg2"
+pkill --newest --signal INT --full "/usr/bin/python3 ${BASEDIR}/tg2"
 wait
 check_value "scapy_display.summary.count: ${NUMBER}"
 cleanup
